532c2732bf31fae068fe0ee51a38d4c430eaffb9
Mina Ghobrial <minamaged113@gmail.com>
2020-11-20 15:56:43 +0200

    Edit package
    - add classifiers
    - add long description format
    - add function to get dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup, find_packages
 from src import __VERSION__
+improt subprocess
+
+def getDependencies(fname):
+    """Get dependencies from requirements file.
+    Get packages installed by pip inside the virual environment.
+    put it in the form of a list
+    
+    Args:
+        fname ([string]): reuirements filename.
+    
+    Returns:
+        dependencyArray ([list]): list of dependendencies for the package
+    """
+    dependencyArray = subprocess.run(["pip", "freeze"], stdout=subprocess.PIPE).stdout.decode('ascii').split('\n')
+    return dependencyArray
 
 setup(
     name="ppt",
@@ -10,8 +25,15 @@ setup(
     license="MIT",
     description="Initializes a python project",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     install_requires=[],
     url="https://github.com/minamaged113/pp-template",
     author="Mina Ghobrial",
     author_email="minamaged113@gmail.com",
+    classifiers= [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux"
+    ]
 )
